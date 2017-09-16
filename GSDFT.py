f50ae4077e2e8410b6b2b86936caaeb5441b092c
Wester <zyzengstorm@gmail.com>
2017-09-16 09:18:32 -0500

    Release v1.1

--- GSDFT.py
+++ GSDFT.py
@@ -54,128 +54,121 @@ class Domain:
         self.domain = domain
         self.Token = Token
         self.ds = []
+        self.domains = []
+        self.flag = 0 
+        self.length = 0
+        self.headers = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.79 Safari/537.36"}
         self.baseUrl = 'https://www.google.com/transparencyreport/jsonp/ct/search?incl_exp=true&incl_sub=true&c=jsonp'
         self.proxies = {
-            'http': 'http://127.0.0.1:8087',
-            'https': 'http://127.0.0.1:8087',
+            'http': 'http://127.0.0.1:1087',
+            'https': 'http://127.0.0.1:1087',
         }
         requests.packages.urllib3.disable_warnings()
 
     def get_domain(self):
-        r = requests.get(self.baseUrl+'&domain='+self.domain+'&token='+self.Token, proxies=self.proxies,verify=False)
+        r = requests.get(self.baseUrl+'&domain='+self.domain+'&token='+self.Token,headers=self.headers,proxies=self.proxies,verify=False)
         # print r.text
         pattern = re.compile(r"jsonp\((.*)\)", re.I|re.X)
         match = pattern.findall(r.text)
         obj = json.loads(match[0])
+        try:
+            if self.flag < 20:
+                self.process(obj['results'])
+                self.flag += 1
+            else:
+                if self.flag == 20:
+                    print "\nOh no,too much subdomains,please give me more patience!"
+                    self.flag +=1
+                else:
+                    pass
+        except:
+            print "something went wrong"
         self.ds.append(obj['results'])
         if 'nextPageToken' in obj.keys():
             self.Token = obj['nextPageToken']
             self.get_domain()
-
+        else:
+            c = Colored()
+            print c.green("\nAll done,here are subdomains for "+ self.domain + "\n")
 
     def run(self):
         try:
             c = Colored()
-            print c.green('\nProcessing...,The result may be numerous.so please wait for a while.')
-            self.get_domain()
+            print c.yellow('Processing...,The result may be numerous.so please wait for a while.')
+            self.get_domain()  
             x = 0
-            domains = []
             while (x<len(self.ds)):
                 for y in self.ds[x]:
-                    domains.append(y['subject'])
+                    self.domains.append(y['subject'])
                 x +=1
             # Remove duplicates
-            domains = list(set(domains))
-
-            # Output the results in a table
-            z = 0
-            width = 20
-            if len(domains)>2:
-                print c.green('\n\nAha,I have found '+str(len(domains))+' subdomains:\n\n')
-                print '-'*(width*3+10)
-                while z <(len(domains)):
-                    if (len(domains) - z) == 1:
-                        print "{} |".format(c.fuchsia("{}")).format(domains[z].ljust(width))
-                    elif (len(domains) - z) == 2:
-                        print "{} | {} |".format(c.fuchsia("{}"), c.cyan("{}")).format(domains[z].ljust(width),
-                                                                         domains[z + 1].ljust(width))
-                    else:
-                        print "{} | {} | {}".format(c.fuchsia("{}"), c.cyan("{}"),
-                                                   c.yellow("{}")).format(domains[z].ljust(width),
-                                                                         domains[z + 1].ljust(width),
-                                                                         domains[z + 2].ljust(width))
-
-
-                    print '-'*(width*3+10)
-                    z +=3
-                print '\n\n'
-            else:
-                if not (len(domains) ==0):
-                    print c.green('\n\nAha,I have found '+str(len(domains))+' subdomains:\n\n')
-                    print '-'*(width+5)
-                    while z <(len(domains)):
-                        print "{0}".format(c.fuchsia("{}")).format(domains[z].ljust(width))
-                        print '-'*(width+5)
-                        z +=1
-                    print '\n\n'
-                else:
-                    print c.red('unfortunately,I can not find subdomains for your conditions...')
-
-            # write into txt file
-            if os.path.exists('log') == False:
-                os.mkdir('log')
-            if(os.name == 'posix'):
-                with open(os.getcwd()+"/log/"+self.domain+'.txt', 'wb') as f:
-                    for i in domains:
-                        f.write(i+'\r\n')
-                        f.flush()
-                f.close()
-            else:
-                with open(os.getcwd()+"\\log\\"+self.domain+'.txt', 'wb') as f:
-                    for i in domains:
-                        f.write(i+'\r\n')
-                        f.flush()
-                f.close()
-
+            self.domains = list(set(self.domains))   
+            #print self.domains
+            for y in self.domains:
+                print y
+            print c.cyan("\nHere are "+str(len(self.domains))+" subdomains for "+self.domain +"\n")
+            self.log()
+            print c.cyan("subdomains has been saved in directory ./"+self.domain+".txt\n")
         except KeyboardInterrupt:
             print "Ctrl-c Pressed，exit..."
             sys.exit(1)
         except:
             print c.red('Sorry,timeout or network is not smooth')
+            
+    def process(self,data):
+        self.length = self.length + len(data)
+        #print self.length
+        print  "["+(self.length//4) * "-" +">" + (50 - self.length//4) * " " + "] " + str(self.length)+"/all done"
+
+    def log(self):
+        # write into txt file
+        if os.path.exists('log') == False:
+            os.mkdir('log')
+        if(os.name == 'posix'):
+            with open(os.getcwd()+"/log/"+self.domain+'.txt', 'wb') as f:
+                for i in self.domains:
+                    f.write(i+'\r\n')
+                    f.flush()
+            f.close()
+        else:
+            with open(os.getcwd()+"\\log\\"+self.domain+'.txt', 'wb') as f:
+                for i in self.domains:
+                    f.write(i+'\r\n')
+                    f.flush()
+            f.close()
 
 #terminal
 class Main(cmd.Cmd):
     '''
-       ____                   _      ____ ____  _        _                       _       _____ _           _           
-  / ___| ___   ___   __ _| | ___/ ___/ ___|| |    __| | ___  _ __ ___   __ _(_)_ __ |  ___(_)_ __   __| | ___ _ __ 
- | |  _ / _ \ / _ \ / _` | |/ _ \___ \___ \| |   / _` |/ _ \| '_ ` _ \ / _` | | '_ \| |_  | | '_ \ / _` |/ _ \ '__|
- | |_| | (_) | (_) | (_| | |  __/___) |__) | |___ (_| | (_) | | | | | | (_| | | | | |  _| | | | | | (_| |  __/ |   
-  \____|\___/ \___/ \__, |_|\___|____/____/|_____\__,_|\___/|_| |_| |_|\__,_|_|_| |_|_|   |_|_| |_|\__,_|\___|_|   
-                    |___/                                                                                          
- 
- Author:Wester@TSRC
+    `______`````______`````_____`````______``
+    /\``___\```/\``___\```/\``__-.``/\``___\`
+    \`\`\__`\``\`\___``\``\`\`\/\`\`\`\``__\`
+    `\`\_____\``\/\_____\``\`\____-``\`\_\```
+    ``\/_____/```\/_____/```\/____/```\/_/```
+    `````````````````````````````````````````
 
+    Author:Wester@Sixtant Security Lab
 
- How to use:
- 
- [find + domain] - list subdomains
- 
- (example command:find mi.com and you don't need to add http/https)
- 
- [cls] - clear screen
- 
- [help] - open this help
+    How to use:
 
- [exit] - exit program
+    [find + domain] - list subdomains
 
+    (example command:find mi.com and you don't need to add http/https)
+
+    [cls] - clear screen
+
+    [help] - open this help
+
+    [exit] - exit program
     '''
     def __init__(self):
         cmd.Cmd.__init__(self)
         co = Colored()
         reload(sys)
         sys.setdefaultencoding('utf-8')
-        self.prompt = co.green("GSDF =>")
-        self.intro = "\nWelcome to "+co.fuchsia("GoogleSSLdomainFinder")+"!\n\nPlease print "+co.red('help')+" to get help,Enjoy!\n\n"
+        self.prompt = co.green("domainfinder =>")
+        self.intro = "Welcome to "+co.fuchsia("GoogleSSLdomainFinder")+"!Please print "+co.red('help')+" to get help,Enjoy!\n"
+    
     def do_EOF(self, line):
         return True
 
@@ -188,7 +181,7 @@ class Main(cmd.Cmd):
         if domain =='':
             print co.red('please input domain')
         elif not re.search(r"^([a-zA-Z0-9]([a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,6}$", domain):
-            print co.red(''+domain+'is illegal')
+            print co.red(''+domain+' is illegal')
         else:
             d = Domain(domain,'CAA=')
             d.run()
